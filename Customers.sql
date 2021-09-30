073d520767b18e71d217f03e503b1cefaa667437
Art3mis <44325474+jmmaheswar@users.noreply.github.com>
2021-09-30 23:46:12 +0530

    Create Customers.sql

---
+++ Customers.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Customers (cus_id INTEGER PRIMARY KEY,name TEXT NOT NULL,address TEXT NOT NULL,email TEXT NOT NULL,pass password NOT NULL);
+-- insert some values
+INSERT INTO Customers VALUES (001, 'Rayan','Mysore','Rayan@gmail.com','asdf1');
+INSERT INTO Customers VALUES (002, 'Raja','Delhi','raja@gmail.com','asdf2' );
+INSERT INTO Customers VALUES (003, 'Ram Shankar','Hassan','ramshankar@gamil.com','asdf3');
+INSERT INTO Customers VALUES (004, 'Savita','Hyderabad','savita@gmail.com','asdf4' );
+INSERT INTO Customers VALUES (005, 'Rangaswamy Muthuswamy','Coimbatore','ranga@gmail.com','asdf5');
+INSERT INTO Customers VALUES (006, 'Paul','Kochi','paul@gmail.com','asdf6');
+INSERT INTO Customers VALUES (007, 'Datta','Kolkata','datta@gmail.com','asdf7');
+INSERT INTO Customers VALUES (008, 'Soma','Chennai','soma@gmail.com','asdf8');
+INSERT INTO Customers VALUES (009, 'Rahul','Bengaluru','Rahul@gmail.com','asdf9');
+INSERT INTO Customers VALUES (010, 'Riya','Pune','riya@gmail.com','asdf10');
+INSERT INTO Customers VALUES (011, 'Rakesh','Mumbai','rakesh@gmail.com','asdf11');
+INSERT INTO Customers VALUES (012, 'Selmon','Mumbai','selmon@gmail.com','asdf12');
+INSERT INTO Customers VALUES (013, 'Sushant','Jaipur','sushant@gmail.com','asdf13');
+INSERT INTO Customers VALUES (014, 'Ramya','Vizag','ramya@gmail.com','asdf14');
+INSERT INTO Customers VALUES (015, 'Shankar','Mangalore','shankar@gmail.com','asdf15');
+INSERT INTO Customers VALUES (016, 'Rani','Kollam','rani@gmail.com','asdf16');
+INSERT INTO Customers VALUES (017, 'Shumi','Dispur','shumi@gmail.com','asdf17');
+INSERT INTO Customers VALUES (018, 'Max','Calicut','max@gmail.com','asdf18');
+INSERT INTO Customers VALUES (019, 'Daniel','Madurai','dan@gmail.com','asdf19');
+INSERT INTO Customers VALUES (020, 'Raksha','Kashmir','raksha@gmail.com','asdf20');
+INSERT INTO Customers VALUES (021, 'Suman','Tanjavur','suman@gmail.com','asdf21');
+INSERT INTO Customers VALUES (022, 'Ranjit','Pondicherry','ranjit@gmail.com','asdf22');
+INSERT INTO Customers VALUES (023, 'Singh','Chandighar','singh@gmail.com','asdf23');
+INSERT INTO Customers VALUES (024, 'Ramesha','Vizag','ramesha@gmail.com','asdf24');
+INSERT INTO Customers VALUES (025, 'Somesha','Bengaluru','somesha@gmail.com','asdf25');
